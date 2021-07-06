1a3aa6bf8ba1bcfdc225ede1e65ae9b1485fe0d9
Louie <louie.navarro@outlook.com>
2021-07-06 20:58:12 +0800

    Add more rules

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,49 +1,39 @@
 import { RouteOptionsValidate } from "hapi";
-import { Description, Schema } from "joi";
+import { Description, GetRuleOptions, Schema } from "joi";
 
-import utils from "./utilities";
+import util from "./utilities";
 
 export const getParameters = (validators: RouteOptionsValidate) => {
-  const parameters: any[] = [];
+  let parameters: any[] = [];
 
   if (validators.headers) {
-    const description = (validators.headers as Schema).describe();
-    traverseParameters(description, parameters, null, "header");
+    parameters = parameters.concat(
+      mapParameters(validators.headers as Schema, "header")
+    );
   }
 
   if (validators.query) {
-    const description = (validators.query as Schema).describe();
-    traverseParameters(description, parameters, null, "query");
+    parameters = parameters.concat(
+      mapParameters(validators.query as Schema, "query")
+    );
   }
 
   if (validators.params) {
-    const description = (validators.params as Schema).describe();
-    traverseParameters(description, parameters, null, "params");
+    parameters = parameters.concat(
+      mapParameters(validators.params as Schema, "params")
+    );
   }
 
   return parameters;
 };
 
-const traverseParameters = (
-  description: Description,
-  parameters: any[],
-  descKey: string,
-  paramIn: string
-) => {
-  if (!description.keys) {
-    parameters.push({
-      in: paramIn,
-      name: descKey,
-      schema: traverseSchema(description, {}),
-      required: isRequired(description),
-    });
-    return;
-  }
-
-  Object.keys(description.keys).forEach((key) => {
-    traverseParameters(description.keys[key], parameters, key, paramIn);
-  });
-};
+const mapParameters = (joiSchema: Schema, paramIn: string) =>
+  Object.entries(joiSchema.describe().keys).map(([key, subDescription]) => ({
+    in: paramIn,
+    name: key,
+    schema: traverseSchema(subDescription, {}),
+    required: isRequired(subDescription),
+  }));
 
 export const getRequestBody = (validators: RouteOptionsValidate) => {
   const schema = validators.payload
@@ -52,45 +42,83 @@ export const getRequestBody = (validators: RouteOptionsValidate) => {
   return { content: { "application/json": { schema } } };
 };
 
-const traverseSchema = (description: Description, apiSchema: any) => {
+const traverseSchema = (joiDescription: Description, apiSchema: any) => {
   try {
-    if (description.keys) {
-      apiSchema.type = "object";
+    if (joiDescription.type === "object") {
+      apiSchema.type = joiDescription.type;
       apiSchema.properties = {};
-      for (let [key, childDescription] of Object.entries(description.keys)) {
-        if (isRequired(childDescription)) {
+      for (let [key, subDescription] of Object.entries(joiDescription.keys)) {
+        if (isRequired(subDescription)) {
           apiSchema.required = apiSchema.required || [];
           apiSchema.required.push(key);
         }
-        apiSchema.properties[key] = traverseSchema(childDescription, {});
+        apiSchema.properties[key] = traverseSchema(subDescription, {});
       }
-      return apiSchema;
-    } else if (description.items) {
-      apiSchema.type = "array";
-      for (let anotherDescription of description.items) {
-        apiSchema.items = traverseSchema(anotherDescription, {});
+    } else if (joiDescription.type === "array") {
+      apiSchema.type = joiDescription.type;
+      for (let subDescription of joiDescription.items) {
+        apiSchema.items = traverseSchema(subDescription, {});
       }
-      return apiSchema;
-    }
+    } else parseFinalSchema(joiDescription, apiSchema);
 
-    defineFinalSchema(description, apiSchema);
     return apiSchema;
   } catch (error) {
     console.error(error);
   }
 };
 
-const defineFinalSchema = (description: Description, apiSchema: any) => {
-  if (description.type === "date") {
-    apiSchema.type = "string";
-    apiSchema.format = "date-time";
-    return;
+const parseFinalSchema = (joiDescription: Description, apiSchema: any) => {
+  // open api 3 does not have date type
+  apiSchema.type = joiDescription.type;
+  if (joiDescription.type === "date") parseDate(joiDescription, apiSchema);
+  if (joiDescription.rules) parseRules(joiDescription, apiSchema);
+
+  const hasEnum =
+    joiDescription.allow && (joiDescription.flags as Record<string, any>)?.only;
+  if (hasEnum) {
+    apiSchema.enum = joiDescription.allow;
   }
-  apiSchema.type = description.type;
 };
 
-const isRequired = (description: Description) => {
-  return utils.getProp(description, "flags.presence") === "required";
+const parseDate = (joiDescription: Description, apiSchema: any) => {
+  const dateFormat = (joiDescription.flags as Record<string, any>)?.format;
+  if (dateFormat === "timestamp" || dateFormat === "javascript") {
+    apiSchema.type = "integer";
+  } else if (dateFormat === "iso") {
+    apiSchema.type = "string";
+    apiSchema.format = "date-time";
+  } else {
+    apiSchema.type = "string";
+    apiSchema.format = "date";
+  }
+};
+
+const parseRules = (joiDescription: Description, apiSchema: any) => {
+  const rulesMap = mapRules(joiDescription.rules);
+  const pattern = rulesMap["pattern"];
+  if (pattern) apiSchema.pattern = pattern.args.regex;
+  const min = rulesMap["min"];
+  if (min) {
+    if (apiSchema.type === "string") apiSchema.minLength = min.args.limit;
+    if (apiSchema.type === "number") apiSchema.min = min.args.limit;
+  }
+  const max = rulesMap["max"];
+  if (max) {
+    if (apiSchema.type === "string") apiSchema.maxLength = max.args.limit;
+    if (apiSchema.type === "number") apiSchema.max = max.args.limit;
+  }
+};
+
+const mapRules = (rules: GetRuleOptions[]) => {
+  let rulesMap = {};
+  rules.forEach((rule: GetRuleOptions) => {
+    rulesMap[rule.name] = rule;
+  });
+  return rulesMap;
+};
+
+const isRequired = (joiDescription: Description) => {
+  return util.getProp(joiDescription, "flags.presence") === "required";
 };
 
 export default {
