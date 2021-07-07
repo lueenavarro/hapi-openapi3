057aee955bc7c46054343ba483ccd1c52d1bc389
Louie <louie.navarro@outlook.com>
2021-07-07 16:31:46 +0800

    Refactor getParameters

--- src/lib/fields.ts
+++ src/lib/fields.ts
@@ -6,25 +6,11 @@ import schema from "./schema";
 import util from "./utilities";
 
 export const getParameters = (validators: RouteOptionsValidate) => {
-  let parameters: any[] = [];
-
-  if (validators.headers) {
-    parameters = parameters.concat(
-      mapParameters(validators.headers as Schema, "header")
-    );
-  }
-
-  if (validators.query) {
-    parameters = parameters.concat(
-      mapParameters(validators.query as Schema, "query")
-    );
-  }
-
-  if (validators.params) {
-    parameters = parameters.concat(
-      mapParameters(validators.params as Schema, "params")
-    );
-  }
+  const parameters = [
+    ...mapParameters(validators.headers as Schema, "header"),
+    ...mapParameters(validators.query as Schema, "query"),
+    ...mapParameters(validators.params as Schema, "params"),
+  ];
 
   if (parameters.length === 0) return undefined;
 
@@ -32,7 +18,8 @@ export const getParameters = (validators: RouteOptionsValidate) => {
 };
 
 const mapParameters = (joiSchema: Schema, paramIn: string) => {
-  if (joiSchema.type !== "object") throw new Error("Invalid Schema");
+  if (!joiSchema) return [];
+  if (joiSchema.type !== "object") throw new Error("Invalid Parameter Schema");
 
   return Object.entries(joiSchema.describe().keys).map(
     ([key, subDescription]) => ({
@@ -46,37 +33,37 @@ const mapParameters = (joiSchema: Schema, paramIn: string) => {
 
 export const getRequestBody = (validators: RouteOptionsValidate) => {
   if (!validators.payload) return undefined;
-  const payloadSchema = schema.traverseSchema(
-    (validators.payload as Schema).describe(),
-    {}
-  );
+  const joiDescription = (validators.payload as Schema).describe();
+  const payloadSchema = schema.traverseSchema(joiDescription, {});
   return { content: { "application/json": { schema: payloadSchema } } };
 };
 
 export const getResponseBody = (routeOptions: RouteOptions) => {
   const statusCodes: Record<number, any> =
+    routeOptions.response.status ||
     routeOptions.plugins["hapi-openapi3"]?.responses;
   if (!statusCodes) return undefined;
 
   const response = {};
   for (let [code, options] of Object.entries(statusCodes)) {
     response[code] = {
-      description: status(parseInt(code, 10)),
+      description: options.description || status(parseInt(code, 10)),
       content: {
         "application/json": {
           schema: schema.traverseSchema(options.payload.describe(), {}),
-          examples:
-            options.examples &&
-            util.mapObject(options.examples, (example: any) => ({
-              value: example,
-            })),
           example: options.example,
+          examples: options.examples && mapExamples(options.examples),
         },
       },
     };
   }
   return response;
 };
+
+const mapExamples = (examples: any) =>
+  util.mapObject(examples, (example: any) => ({
+    value: example,
+  }));
 
 export default {
   getParameters,
